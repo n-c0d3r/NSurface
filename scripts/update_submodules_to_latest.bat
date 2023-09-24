38bc2b3864be15695916c7d3b5b1ca1292291a8c
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-09-24 18:49:10 +0700

    update scripts

--- scripts/update_submodules_to_latest.bat
+++ scripts/update_submodules_to_latest.bat
@@ -6,4 +6,4 @@ set SOURCE_DIR=%0\..\..\
 
 cd %SOURCE_DIR%
 git submodule update --init
-git submodule update
+git submodule update --remote
--- scripts/update_submodules_to_latest.sh
+++ scripts/update_submodules_to_latest.sh
@@ -4,4 +4,4 @@ SOURCE_DIR="$(dirname "$0")/.."
 
 cd "$SOURCE_DIR"
 git submodule update --init
-git submodule update
+git submodule update --remote
