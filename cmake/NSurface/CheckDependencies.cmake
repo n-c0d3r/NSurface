7f02abeed06e09a79da8a28eee46dffb793f3988
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-09-24 17:42:18 +0700

    update cmake

--- cmake/NSurface/CheckDependencies.cmake
+++ cmake/NSurface/CheckDependencies.cmake
@@ -16,6 +16,16 @@ if(NOT TARGET ncpp)
         endif()
     endif()    
 endif()
+# check for nevent
+if(NOT TARGET ncpp)
+    if(NOT EXISTS "${NSURFACE_SUBMODULES_DIR}/nevent")
+        if(WIN32)
+            execute_process(COMMAND "${NSURFACE_SCRIPTS_DIR}/check_submodules.bat")
+        elseif(UNIX)
+            execute_process(COMMAND "${NSURFACE_SCRIPTS_DIR}/check_submodules.sh")
+        endif()
+    endif()    
+endif()
 
 
 
--- cmake/NSurface/ConfigureDependencies.cmake
+++ cmake/NSurface/ConfigureDependencies.cmake
@@ -11,6 +11,9 @@ find_package(Doxygen)
 if(NOT TARGET ncpp)
     add_subdirectory("${NSURFACE_SUBMODULES_DIR}/ncpp" "${NSURFACE_BINARY_DIR}/submodules/ncpp")
 endif()
+if(NOT TARGET nevent)
+    add_subdirectory("${NSURFACE_SUBMODULES_DIR}/nevent" "${NSURFACE_BINARY_DIR}/submodules/nevent")
+endif()
 
 
 
