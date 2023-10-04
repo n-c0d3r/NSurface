a3d0575e71058210f656efec88c0290760dc9006
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-10-04 22:46:22 +0700

    update submodules

--- cmake/NSurface/ConfigureDependencies.cmake
+++ cmake/NSurface/ConfigureDependencies.cmake
@@ -9,10 +9,15 @@ message(STATUS "<NSurface::ConfigureDependencies> Start configuring dependencies
 find_package(Doxygen)
 
 if(NOT TARGET ncpp)
-    add_subdirectory("${NSURFACE_SUBMODULES_DIR}/ncpp" "${NSURFACE_BINARY_DIR}/submodules/ncpp")
+    add_subdirectory("${NSURFACE_SUBMODULES_DIR}/NCPP" "${NSURFACE_BINARY_DIR}/submodules/NCPP")
 endif()
+
+if(NOT TARGET ntask_system)
+    add_subdirectory("${NSURFACE_SUBMODULES_DIR}/NTaskSystem" "${NSURFACE_BINARY_DIR}/submodules/NTaskSystem")
+endif()
+
 if(NOT TARGET nevent)
-    add_subdirectory("${NSURFACE_SUBMODULES_DIR}/nevent" "${NSURFACE_BINARY_DIR}/submodules/nevent")
+    add_subdirectory("${NSURFACE_SUBMODULES_DIR}/NEvent" "${NSURFACE_BINARY_DIR}/submodules/NEvent")
 endif()
 
 
